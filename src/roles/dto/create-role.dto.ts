d59e070e3e8c5e1f2aee1af82f19d7ebbe16ad70
LePhanHaiLong <longlph.23it@gmail.com>
2025-08-28 23:53:27 +0700

    update api create role

--- src/roles/dto/create-role.dto.ts
+++ src/roles/dto/create-role.dto.ts
@@ -1,4 +1,5 @@
-import { IsNotEmpty } from "class-validator";
+import { IsArray, IsMongoId, IsNotEmpty } from "class-validator";
+import mongoose from "mongoose";
 
 export class CreateRoleDto {
   @IsNotEmpty({ message: "Name is required" })
@@ -6,4 +7,10 @@ export class CreateRoleDto {
 
   @IsNotEmpty({ message: "Description is required" })
   description: string;
+
+
+  @IsNotEmpty({ message: 'permissions is required' })
+  @IsMongoId({ each: true, message: "each permission must be a mongo object id" })
+  @IsArray({ message: 'permissions must be an array' })
+  permissions: mongoose.Types.ObjectId[];
 }
--- src/roles/schema/role.schema.ts
+++ src/roles/schema/role.schema.ts
@@ -1,5 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { HydratedDocument } from 'mongoose';
+import { Permission } from 'src/permissions/schema/permission.schema';
 
 export type RoleDocument = HydratedDocument<Role>;
 
@@ -10,6 +11,9 @@ export class Role {
 
   @Prop({ required: true })
   description: string;
+
+  @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: Permission.name }] })
+  permissions: Permission[];
 
   @Prop()
   createdAt: Date;
